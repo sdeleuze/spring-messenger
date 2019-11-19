6c7e436b2f6c137119d9785df7f0d11797a082b8
Sébastien Deleuze <sdeleuze@pivotal.io>
2019-11-19 11:29:00 +0100

    Upgrade to Kotlin 1.3.60

--- settings.gradle.kts
+++ settings.gradle.kts
@@ -17,7 +17,7 @@ pluginManagement {
 		}
 	}
 	plugins {
-		val kotlinVersion = "1.3.50"
+		val kotlinVersion = "1.3.60"
 		id("org.jetbrains.kotlin.js") version kotlinVersion apply false
 		id("org.jetbrains.kotlin.multiplatform") version kotlinVersion  apply false
 		id("org.jetbrains.kotlin.jvm") version kotlinVersion apply false
