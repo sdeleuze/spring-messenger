3cdd2a5b5e23636276928af4d9da924184802ab7
Sébastien Deleuze <sdeleuze@pivotal.io>
2019-11-19 12:01:46 +0100

    Rename MessengerApplicationTests to BackendApplicationTests

--- backend/src/test/kotlin/messenger/backend/BackendApplicationTests.kt
+++ backend/src/test/kotlin/messenger/backend/BackendApplicationTests.kt
@@ -4,7 +4,7 @@ import org.junit.jupiter.api.Test
 import org.springframework.boot.test.context.SpringBootTest
 
 @SpringBootTest
-class MessengerApplicationTests {
+class BackendApplicationTests {
 
 	@Test
 	fun contextLoads() {
